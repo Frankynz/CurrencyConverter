b29c2f7315bf335854ac14caca97628943fff490
Franky <Rewq1999.11.09@gmail.com>
2020-10-07 15:06:05 +0300

    attempt 5

--- src/main/java/ru/lasttask/javamentor/Domain.java
+++ src/main/java/ru/lasttask/javamentor/Domain.java
@@ -1,23 +1,24 @@
 package ru.lasttask.javamentor;
 
 import org.joda.money.CurrencyUnit;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-
-import java.io.IOException;
-import java.math.BigDecimal;
+import org.joda.money.Money;
 
 public class Domain {
-    public static void main(String[] args) throws IOException, JSONException {
-
-        System.out.println("Converter Test\n"+ CurrencyConverter.convertToRub(BigDecimal.valueOf(1.9), CurrencyUnit.USD));
-        System.out.println("Converter Test\n"+ CurrencyConverter.convertToRub(BigDecimal.valueOf(1.239),CurrencyUnit.USD));
-        System.out.println("Converter Test\n"+ CurrencyConverter.convertToRub(BigDecimal.valueOf(1.1231239),CurrencyUnit.USD));
-        System.out.println("Converter Test\n"+ CurrencyConverter.convertUSDtoRUB(BigDecimal.valueOf(1)));
-        System.out.println("Converter Test\n"+ CurrencyConverter.convertToRub(BigDecimal.valueOf(2),CurrencyUnit.EUR));
-        System.out.println("Converter Test\n"+ CurrencyConverter.convertEURtoRUB(BigDecimal.valueOf(2)));
 
 
+    public static void main(String[] args) throws Exception {
 
+        CurrencyConverterService service = new CurrencyConverterService();
+
+        Money eur = Money.of(CurrencyUnit.of("EUR"), 1000d);
+        Money usd = Money.of(CurrencyUnit.of("USD"), 1000d);
+        Money kgs = Money.of(CurrencyUnit.of("KGS"), 1000d);
+        Money rub = Money.of(CurrencyUnit.of("RUB"), 1000d);
+
+        System.out.println("Converter Test\n" + service.convertToRub(eur));
+        System.out.println("Converter Test\n" + service.convertToRub(usd));
+        System.out.println("Converter Test\n" + service.convertToRub(kgs));
+        System.out.println("Converter Test\n" + service.convertToRub(rub));
 
     }
 }
